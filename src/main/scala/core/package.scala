98a180f3c2ea4de7f39ecfbbc5d9089305678163
gutscdav000 <gutscdav000@gmail.com>
2020-05-04 19:40:13 -0400

    got sbt building, but struggling with sttp and zio issues.

---
+++ src/main/scala/core/package.scala
@@ -0,0 +1,17 @@
+import org.http4s.{EntityBody, HttpRoutes}
+import sttp.tapir.Endpoint
+import sttp.tapir.server.ServerEndpoint
+import sttp.tapir.server.http4s.Http4sServerOptions
+import zio.{IO, Task}
+
+package object core {
+  // extension methods for ZIO; not a strict requirement, but they make working with ZIO much nicer
+//  implicit class ZioEndpoint[I, E, O](e: Endpoint[I, E, O, EntityBody[Task]]) {
+//    def toZioRoutes(logic: I => IO[E, O])(implicit serverOptions: Http4sServerOptions[Task]): HttpRoutes[Task] = {
+//      import sttp.tapir.server.http4s._
+//      e.toRoutes(i => logic(i).either)
+//    }
+//
+//    def zioServerLogic(logic: I => IO[E, O]): ServerEndpoint[I, E, O, EntityBody[Task], Task] = ServerEndpoint(e, logic(_).either)
+//  }
+}
