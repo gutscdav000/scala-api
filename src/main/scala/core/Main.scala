b15b94e8f7e6514c1ed346f9b426bed61fb7c19b
gutscdav000 <gutscdav000@gmail.com>
2020-05-05 08:10:29 -0400

    great pain much hate, striped the project and created a simple route.

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -1,96 +1,32 @@
 package core
 package core.fp
 
-import java.time.Instant
-import java.util.UUID
-
-import doobie._
-import doobie.implicits._
-import doobie.postgres.implicits._
-import doobie.implicits.legacy.instant._
+//import cats.effect.{ExitCode, IO, IOApp}
+//import org.http4s.HttpRoutes
+//import org.http4s.dsl.impl.Root
+import cats.effect._
 import cats.implicits._
-import com.typesafe.scalalogging.StrictLogging
-import sttp.tapir._
-import sttp.tapir.json.circe._
-import io.circe.generic.auto._
-import org.http4s.{EntityBody, HttpRoutes}
-import org.http4s.server.Router
-import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.syntax.kleisli._
-import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.server.http4s.Http4sServerOptions
-import sttp.tapir.swagger.http4s.SwaggerHttp4s
-import zio._
-import zio.interop.catz._
-import zio.interop.catz.implicits._
+import org.http4s.HttpRoutes
+import org.http4s.syntax._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.blaze._
+import scala.concurrent.ExecutionContext.global
 
 
-object Main {// extends App with StrictLogging{
- def main(args: Array[String]): Unit = {
-   println("Hello Scala")
- }
-//  def upsertLanguage(name: String): ConnectionIO[UUID] =
-//    sql"""INSERT INTO languages(id, name) VALUES (${UUID.randomUUID()}, $name)
-//         |ON CONFLICT ON CONSTRAINT languages_name_key
-//         |DO UPDATE SET name=languages.name RETURNING (id)""".stripMargin.update
-//      .withUniqueGeneratedKeys[UUID]("id")
-//
-//
-//
-//  def insertLike(who: String, what: String): ConnectionIO[Unit] = {
-//    upsertLanguage(what).flatMap { languageId =>
-//      sql"""INSERT INTO likes(id, language_id, username, created)
-//           |VALUES(${UUID.randomUUID()}, $languageId, $who, ${Instant
-//        .now()})""".stripMargin.update.run.void
-//    }
-//  }
-//
-//  //
-//
-//  case class LikeData(userName: String, languageName: String)
-//  val likeEndpoint: Endpoint[(String, LikeData), String, Unit, Nothing] = endpoint.post
-//    .in(auth.bearer)
-//    .in("api" / "like")
-//    .in(jsonBody[LikeData])
-//    .errorOut(stringBody)
-//
-//
-//  val transactor: Transactor[Task] = Transactor.fromDriverManager[Task](
-//    "org.postgresql.Driver",
-//    "jdbc:postgresql:fp",
-//    "postgres",
-//    ""
-//  )
-//
-//    val likeRoute: HttpRoutes[Task] = likeEndpoint.toZioRoutes {
-//      case (token, likeData) =>
-//        if (token == "1234") {
-//          insertLike(likeData.userName, likeData.languageName).transact(transactor).mapError { t =>
-//            logger.error("Exception when executing a query", t)
-//            "Internal server error"
-//          }
-//        } else {
-//          IO.fail("Invalid token")
-//        }
-//    }
-//
-//  val yaml: String = {
-//    import sttp.tapir.docs.openapi._
-//    import sttp.tapir.openapi.circe.yaml._
-//    List(likeEndpoint).toOpenAPI("Best languages", "1.0").toYaml
-//  }
-//
-//  val swaggerRoute: HttpRoutes[Task] = new SwaggerHttp4s(yaml).routes[Task]
-//
-//  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = ZIO.runtime.flatMap {
-//
-//    val serve = BlazeServerBuilder[Task]().bindHttp(8080, "localhost")
-//      .withHttpApp(Router("/" -> (likeRoute <+> swaggerRoute)).orNotFound)
-//      .serve
-//      .compile
-//      .drain
-//      .map(_ => 0)
-//      .catchAll(t => UIO(logger.error("Exception when starting server", t)).map(_ => 1))
-//    serve
-//  }
+object Main extends IOApp {
+
+  val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name.")
+  }.orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO](ConcurrentEffect[IO], cats.effect.Timer[cats.effect.IO])
+      .bindHttp(8080, "localhost")
+      .withHttpApp(helloWorldService)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
