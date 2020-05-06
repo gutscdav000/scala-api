e6dd3b5046a3ac8f1721c2d7477ed5ae185f6a4a
gutscdav000 <gutscdav000@gmail.com>
2020-05-05 20:46:09 -0400

    preliminary DB companion objects returnign DB transactor.

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -4,27 +4,46 @@ package core.fp
 //import cats.effect.{ExitCode, IO, IOApp}
 //import org.http4s.HttpRoutes
 //import org.http4s.dsl.impl.Root
+import cats.data.Kleisli
 import cats.effect._
 import cats.implicits._
+import com.typesafe.scalalogging.StrictLogging
+import doobie.util.transactor.Transactor
 import org.http4s.HttpRoutes
 import org.http4s.syntax._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 import org.http4s.server.blaze._
+
 import scala.concurrent.ExecutionContext.global
+// custom
+import model.DB
 
 
-object Main extends IOApp {
+object Main extends IOApp with StrictLogging {
 
-  val helloWorldService = HttpRoutes.of[IO] {
+  val httpRoutes = HttpRoutes.of[IO] {
     case GET -> Root / "hello" / name =>
+      logger.debug("***hello world service***")
       Ok(s"Hello, $name.")
+    case GET -> Root / "second" / name =>
+      logger.debug("***second service***")
+      logger.debug(s"testing transactor ${transactor.toString} ${transactor} type: ${transactor.transactor.getClass}")
+      Ok(s"second, $name.")
   }.orNotFound
+
+  val transactor: DB = DB(
+        "org.postgresql.Driver",
+        "jdbc:postgresql:ApplicationDb",
+        "postgres",
+        "postgres"
+  )
+
 
   def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO](ConcurrentEffect[IO], cats.effect.Timer[cats.effect.IO])
       .bindHttp(8080, "localhost")
-      .withHttpApp(helloWorldService)
+      .withHttpApp(httpRoutes)
       .serve
       .compile
       .drain
